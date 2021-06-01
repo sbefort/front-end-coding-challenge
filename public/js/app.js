a74141ba07609f568f555d8a0d6d1a04c8ffaded
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2021-06-01 11:09:13 -0400

    Update to New DC-based Test
    Switch to using DC assets.  Added new test instructions and sample video.  Update readme.  Cleanup starter markup.  Remove jQuery.  Add no-cache header to node-static npm task for easier development.  Misc. other minor tweaks.

--- public/js/app.js
+++ public/js/app.js
@@ -2,21 +2,15 @@ export default class TagBrowserWidget {
   constructor(config) {
     this.config = config;
 
-    this.fetchData()
-      //use .bind because native promises change the "this" context
+    fetch('/js/data.json')
+      .then((response) => response.json())
+      // use .bind because native promises change the "this" context
       .then(this.setData.bind(this))
       .then(this.getElements.bind(this))
       .then(this.bindEventListeners.bind(this))
-      .then(this.render.bind(this));
+      .then(this.renderTagList.bind(this));
 
     console.log('Widget Instance Created');
-  }
-
-  fetchData() {
-    return new Promise((resolve, reject) => {
-      //ajax the data and resolve the promise when it comes back
-      $.get('/js/data.json', resolve);
-    });
   }
 
   setData(data) {
@@ -26,23 +20,21 @@ export default class TagBrowserWidget {
 
   getElements() {
     this.tagList = this.config.element.querySelectorAll('.tag-list')[0];
-
-    //find and store other elements you need
+    // find and store other elements you need
   }
 
   bindEventListeners() {
     this.tagList.addEventListener('click', this.tagListClicked.bind(this));
-
-    //bind the additional event listener for clicking on a series title
+    // bind the additional event listener for clicking on a series title
   }
 
-  render() {
-    //render the list of tags from this.data into this.tagList
+  renderTagList() {
+    // render the list of tags from this.data into this.tagList
   }
 
   tagListClicked(event) {
     console.log('tag list (or child) clicked', event);
-    //check to see if it was a tag that was clicked and render
-    //the list of series that have the matching tags
+    // check to see if it was a tag that was clicked and render
+    // the list of series that have the matching tags
   }
 }
